acbaaf372ce18c523f372ed8824186866e0d90d6
suizhikuo <suizhikuo@126.com>
2013-11-30 19:00:23 +0800

    代码上传
    代码上传

---
+++ GetHTMLContent/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GetHTMLContent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 94);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(791, 403);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(256, 50);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 509);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
+
---
+++ GetHTMLContent/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using DotNet.Utilities;
+using System.IO;
+
+namespace GetHTMLContent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string PageUrl = "http://www.cnblogs.com/suizhikuo"; //需要获取源代码的网页
+            WebClient wc = new WebClient(); // 创建WebClient实例提供向URI 标识的资源发送数据和从URI 标识的资源接收数据
+            wc.Credentials = CredentialCache.DefaultCredentials; // 获取或设置用于对向 Internet 资源的请求进行身份验证的网络凭据。           
+            Encoding enc = Encoding.GetEncoding("utf-8"); // 如果是乱码就改成 utf-8 / GB2312
+            Byte[] pageData = wc.DownloadData(PageUrl); // 从资源下载数据并返回字节数组。
+            string resultText = enc.GetString(pageData); // 输出字符串(HTML代码) 
+
+            //htmlDcoument对象用来访问Html文档s
+            HtmlAgilityPack.HtmlDocument hd = new HtmlAgilityPack.HtmlDocument();//需要引用HtmlAgilityPack.dll
+            //加载Html文档
+            hd.LoadHtml(resultText);
+            string str = hd.GetElementbyId("leftmenu").OuterHtml;//指定的id
+            this.txtResult.Text = str;
+        }
+    }
+}
